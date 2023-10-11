9813632cfbf4506dd267c5259e3a354c991df674
Anas-Abuhatab-Rummantech <anas.abuhatab@rumman.tech>
2023-10-11 04:31:05 +0300

    fix build

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import "../styles/components/content.css";
 import BoardCard from "./BoardCard";
-import data from "../data.js";
+// import data from "../data.js";
 import BaseCard from "./BaseCard";
 
 export default function Content() {
@@ -29,7 +29,47 @@ export default function Content() {
       };
     });
   };
-
+  // for building purposes
+  const data = {
+    data: [
+      {
+        id: 1,
+        title: "First Task",
+        status: "To-Do",
+        priority: "Low",
+        dueDate: "2023-10-17",
+        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+      },
+      {
+        id: 2,
+        title: "Second Task",
+        status: "To-Do",
+        priority: "High",
+        dueDate: "2023-10-10",
+        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+      },
+      {
+        id: 3,
+        title: "Third Task",
+        status: "To-Do",
+        priority: "Medium",
+        dueDate: "2023-10-12",
+        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+      },
+      {
+        id: 4,
+        title: "4Th Task",
+        status: "To-Do",
+        priority: "Highest",
+        dueDate: "2023-10-10",
+        description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`,
+      },
+    ],
+  };
   if (!localStorage.getItem("userData")) {
     localStorage.setItem("userData", JSON.stringify([...data.data]));
   }
